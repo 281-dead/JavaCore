a8abfdcb5e7787b5f60901e943bb4cb651694b7a
cuong28 <hoangcuong45c@gmail.com>
2024-09-13 10:13:50 +0700

    init project

---
+++ src/JavaCore/HelloWord.java
@@ -0,0 +1,11 @@
+package JavaCore;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+		System.out.println("Hello word");
+
+	}
+}
